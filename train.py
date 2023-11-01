6366b4055bc1d8420b51c08c47022f77f0a66b0a
Jon Durbin <jon@jondurbin.com>
2023-11-01 12:47:28 -0400

    Update train.py

--- train.py
+++ train.py
@@ -375,7 +375,7 @@ def get_accelerate_model(args, checkpoint_dir):
     tokenizer = AutoTokenizer.from_pretrained(
         args.model_name_or_path,
         cache_dir=args.cache_dir,
-        use_fast=True, #False,
+        use_fast=False,
         padding_side="right",
         tokenizer_type='llama' if 'llama' in args.model_name_or_path else None, # Needed for HF name change
         trust_remote_code=args.trust_remote_code,
